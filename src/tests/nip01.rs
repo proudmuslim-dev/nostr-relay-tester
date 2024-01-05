a4927022e8a2341ad83c50807ab2a29f871b268b
Proudmuslim <proudmuslim-dev@protonmail.com>
2024-01-04 16:34:50 -0800

    wip(nip01): Fix bugs in subscription tests

--- src/tests/nip01.rs
+++ src/tests/nip01.rs
@@ -1,5 +1,7 @@
 use color_eyre::eyre::anyhow;
-use nostr::{secp256k1::XOnlyPublicKey, EventId, Kind, RelayMessage, SubscriptionId, Timestamp};
+use nostr::{
+    secp256k1::XOnlyPublicKey, ClientMessage, EventId, JsonUtil, Kind, RelayMessage, SubscriptionId, Timestamp,
+};
 use nostr_sdk::{Client as NostrClient, InternalSubscriptionId, Relay, RelayPoolNotification};
 use once_cell::sync::Lazy;
 use tokio::sync::broadcast::Receiver;
@@ -13,7 +15,7 @@ use crate::{
     },
 };
 
-static PUBLISH_TEST_SUBSCRIPTION_ID: Lazy<SubscriptionId> = Lazy::new(|| SubscriptionId::new("publish_test"));
+static PUBLISH_TEST_INTERNAL_SUBSCRIPTION_ID: Lazy<SubscriptionId> = Lazy::new(|| SubscriptionId::new("publish_test"));
 
 pub async fn test(client: &NostrClient, relay: &Relay) -> TestReport {
     let span = span!(Level::INFO, "nip01: publishing event");
@@ -21,8 +23,8 @@ pub async fn test(client: &NostrClient, relay: &Relay) -> TestReport {
 
     let mut errors = vec![];
 
-    test_establish_publish_subscription(relay, client.keys().await.public_key(), &mut errors).await;
-
+    let external_subscription_id =
+        test_establish_publish_subscription(relay, client.keys().await.public_key(), &mut errors).await;
     let published_id = match client.publish_text_note("nostr-relay-tester: nip01", vec![]).await {
         Ok(id) => {
             info!("successfully published: {id}");
@@ -34,11 +36,12 @@ pub async fn test(client: &NostrClient, relay: &Relay) -> TestReport {
         }
     };
 
-    test_receive_published_note_from_subscription(published_id, client.notifications(), &mut errors).await;
+    if let Some(id) = external_subscription_id {
+        let notif_stream = client.notifications();
+        test_receive_published_note_from_subscription(published_id, id.clone(), notif_stream, &mut errors).await;
 
-    relay
-        .send_msg(nostr::ClientMessage::Close(PUBLISH_TEST_SUBSCRIPTION_ID.clone()), None)
-        .await;
+        relay.send_msg(ClientMessage::Close(id), None).await;
+    }
 
     drop(_s);
     drop(span);
@@ -53,12 +56,16 @@ pub async fn test(client: &NostrClient, relay: &Relay) -> TestReport {
     TestReport::Passed(Nips::Nip01)
 }
 
-async fn test_establish_publish_subscription(relay: &Relay, pubkey: XOnlyPublicKey, errors: &mut Errors) {
+async fn test_establish_publish_subscription(
+    relay: &Relay,
+    pubkey: XOnlyPublicKey,
+    errors: &mut Errors,
+) -> Option<SubscriptionId> {
     let subscription_result = relay
         .subscribe_with_internal_id(
-            InternalSubscriptionId::Custom(PUBLISH_TEST_SUBSCRIPTION_ID.to_string()),
+            InternalSubscriptionId::Custom(PUBLISH_TEST_INTERNAL_SUBSCRIPTION_ID.to_string()),
             vec![nostr::Filter::new()
-                .pubkey(pubkey)
+                .author(pubkey)
                 .kind(Kind::TextNote)
                 .since(Timestamp::now())],
             None,
@@ -66,23 +73,36 @@ async fn test_establish_publish_subscription(relay: &Relay, pubkey: XOnlyPublicK
         .await;
 
     match subscription_result {
-        Ok(()) => info!("successfully established new event subscription"),
+        Ok(()) => {
+            info!("successfully established new event subscription");
+            let external_id = relay
+                .subscriptions()
+                .await
+                .get(&InternalSubscriptionId::Custom(
+                    PUBLISH_TEST_INTERNAL_SUBSCRIPTION_ID.to_string(),
+                ))?
+                .id();
+
+            Some(external_id)
+        }
         Err(e) => {
             log_and_store_error(anyhow!("failed to create new events subscription: {e}"), errors);
+            None
         }
     }
 }
 
 // TODO: Complete checks
 async fn test_receive_published_note_from_subscription(
-    published_id: Option<EventId>,
+    published_event_id: Option<EventId>,
+    external_subscription_id: SubscriptionId,
     mut notifications_stream: Receiver<RelayPoolNotification>,
     errors: &mut Errors,
 ) {
     while let Ok(notification) = notifications_stream.recv().await {
         if let RelayPoolNotification::Message { message, .. } = notification {
             let mut check_subscription_id = |subscription_id: &SubscriptionId| {
-                if !subscription_id.eq(&PUBLISH_TEST_SUBSCRIPTION_ID) {
+                if !subscription_id.eq(&external_subscription_id) {
                     log_and_store_error(anyhow!("incorrect subscription id: {subscription_id}"), errors);
                 }
             };
@@ -91,7 +111,7 @@ async fn test_receive_published_note_from_subscription(
                 RelayMessage::Event { subscription_id, event } => {
                     check_subscription_id(&subscription_id);
 
-                    if matches!(published_id, Some(id) if id.eq(&event.id)) {
+                    if matches!(published_event_id, Some(id) if id.eq(&event.id)) {
                         info!("received published event from subscription: {event:#?}");
                         break;
                     }
